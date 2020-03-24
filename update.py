91a7a595c4001674851d49aa2fd04f1d7fa54667
Camillo Bruni <cbruni@chromium.org>
2020-03-24 16:31:41 +0100

    Improve update.py and build turbolizer versions
    - clean git dir before rsync'ing to avoid more stale files
    - sort versions
    - build turbolizer if turbolizer/build does not exist
    - force build turbolizer for the last 4 versions

--- update.py
+++ update.py
@@ -37,8 +37,15 @@ git('fetch', '--all')
 
 
 step('List branches')
-BRANCHES = git('branch', '--all', '--list', '*lkgr', '--format=%(refname)', capture=True).split()
-BRANCHES = list(map(lambda ref: ref.split('/')[-1], BRANCHES))
+BRANCHES = git('branch', '--all', '--list', '*-lkgr', '--format=%(refname)', capture=True).split()
+BRANCHES = list(set(map(lambda ref: ref.split('/')[-1], BRANCHES)))
+
+# Sort branches from old to new:
+BRANCHES.sort(key=lambda branch: list(map(int, branch.split('-')[0].split('.'))))
+BRANCHES.append('lkgr')
+
+# List of branches that have potential back-merges and thus need updates:
+BRANCHES_FORCE_BUILDS = set(BRANCHES[-4:])
 print(BRANCHES)
 
 
@@ -46,25 +53,27 @@ print(BRANCHES)
 for branch in BRANCHES:
     step(f'Importing web tools from branch: {branch}')
     if branch == 'lkgr':
-        branch_name = 'head'
         version_name = 'head'
     else:
         branch_name = branch.split('-')[0]
         version_name = f'v{branch_name}'
-    branch_dir = DESTINATION / version_name
+    branch_dir = DESTINATION / version_name 
     branch_dir.mkdir(exist_ok=True)
     git('switch', '--force', '--detach', f'remotes/origin/{branch}')
+    git('clean', '--force', '-d')
     source = GIT_DIR / 'tools'
     run('rsync', '--itemize-changes', f'--include-from={WHITELIST}',
             '--exclude=*', '--recursive', 
             '--checksum', f'{source}{os.sep}', f'{branch_dir}{os.sep}')
     turbolizer_dir = branch_dir / 'turbolizer'
-    if (turbolizer_dir / 'package.json').exists() \
-            and not (turbolizer_dir / 'build').exists():
-        try:
+    if (turbolizer_dir / 'package.json').exists():
+        force_build = branch in BRANCHES_FORCE_BUILDS
+        if force_build or not (turbolizer_dir / 'build').exists():
             step(f'Building turbolizer: {turbolizer_dir}')
-            run('npm', 'i', cwd=turbolizer_dir)
-            run('npm', 'run-script', 'build', cwd=turbolizer_dir)
-        except Exception as e:
-            print(f'Error occured: {e}')
+            run('rm', '-rf', turbolizer_dir / 'build')
+            try:
+                run('npm', 'i', cwd=turbolizer_dir)
+                run('npm', 'run-script', 'build', cwd=turbolizer_dir)
+            except Exception as e:
+                print(f'Error occured: {e}')
 
