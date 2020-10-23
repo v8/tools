0087826598464abb97e05dd6e4d5985f36f50370
Camillo Bruni <cbruni@chromium.org>
2020-10-23 18:45:50 +0200

    update

--- v8.5/turbolizer/build/turbolizer.js
+++ v8.5/turbolizer/build/turbolizer.js
@@ -5765,8 +5765,6 @@
   var saturday = weekday(6);
 
   var sundays = sunday.range;
-  var mondays = monday.range;
-  var thursdays = thursday.range;
 
   var month = newInterval(function(date) {
     date.setDate(1);
@@ -5856,8 +5854,6 @@
   var utcSaturday = utcWeekday(6);
 
   var utcSundays = utcSunday.range;
-  var utcMondays = utcMonday.range;
-  var utcThursdays = utcThursday.range;
 
   var utcMonth = newInterval(function(date) {
     date.setUTCDate(1);
