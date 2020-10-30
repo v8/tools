8823fc916e03cc2f899d9e71deebec031938c843
Camillo Bruni <cbruni@chromium.org>
2020-10-30 18:54:51 +0100

    filter branches before updating

--- update.py
+++ update.py
@@ -50,6 +50,12 @@ with Step(f'Getting V8 checkout in: {V8_GIT}'):
     if not V8_GIT.exists():
         run('git', 'clone', '--depth=1', 'https://github.com/v8/v8.git', V8_GIT)
 
+def map_branch_name(branch):
+    version = branch.split('-')[0]
+    if version == 'lkgr':
+        return 'head'
+    return f"v{version}"
+
 with Step('List Branches'):
     # Find the refs and SHA of all remote branches.
     BRANCHES = git('ls-remote', '--heads', 'origin', capture=True).rstrip().split("\n")
@@ -57,47 +63,45 @@ with Step('List Branches'):
     BRANCHES = [(branch.split('/')[-1], sha) for sha,branch in BRANCHES]
     # Only keep release branches
     BRANCHES = filter(lambda branch_and_sha:branch_and_sha[0].endswith("lkgr"), BRANCHES)
-    BRANCHES = [(branch.split('-')[0], branch, sha) for branch,sha in BRANCHES]
+    BRANCHES = [(map_branch_name(branch), branch, sha) for branch,sha in BRANCHES]
     
     # Sort branches from old to new:
     def branch_sort_key(version_branch_sha): 
-      if version_branch_sha[0] == 'lkgr':
-        return (float("inf"),)
-      return tuple(map(int, version_branch_sha[0].split('.')))
+        if version_branch_sha[0] == 'head':
+            return (float("inf"),)
+        return tuple(map(int, version_branch_sha[0][1:].split('.')))
 
     BRANCHES.sort(key=branch_sort_key)
     print(BRANCHES)
-    
+   
+def filter_by_stamp(values):
+    version,branch,sha = values
+    stamp = OUT_DIR / version / '.sha'
+    if not stamp.exists():
+        print(f'{version} needs update: no stamp file {stamp}')
+        return True
+    stamp_mtime = stamp.stat().st_mtime
+    if stamp_mtime <= Path(__file__).stat().st_mtime:
+        print(f'{version} needs update: stamp file older than update script')
+        return True
+    stamp_sha = stamp.read_text()
+    if stamp_sha != sha:
+        print(f'{version} needs update: stamp SHA does not match branch SHA ({stamp_sha} vs. {sha})')
+        return True
+    return False
+
+
 with Step("Fetch Filtered Branches"):
     # Fetch only the required branches
+    BRANCHES = list(filter(filter_by_stamp, BRANCHES))
     git("fetch", "--depth=1", "origin", *(branch for version,branch,sha in BRANCHES))
 
 for version,branch,sha in BRANCHES:
     with Step(f'Generating Branch: {branch}'):
-        if version == 'lkgr':
-            version = 'head'
         branch_dir = OUT_DIR / version
         branch_dir.mkdir(exist_ok=True)
 
         stamp = branch_dir / '.sha'
-
-        def needs_update():
-            if not stamp.exists():
-                print(f'Needs update: no stamp file')
-                return True
-            stamp_mtime = stamp.stat().st_mtime
-            if stamp_mtime <= Path(__file__).stat().st_mtime:
-                print(f'Needs update: stamp file older than update script')
-                return True
-            stamp_sha = stamp.read_text()
-            if stamp_sha != sha:
-                print(f'Needs update: stamp SHA does not match branch SHA ({stamp_sha} vs. {sha})')
-                return True
-            return False
-
-        if not needs_update():
-            print(f'Docs already up-to-date.')
-            continue
         stamp.write_text(sha)
 
         git('switch', '--force', '--detach', sha)
@@ -113,6 +117,8 @@ for version,branch,sha in BRANCHES:
                 try:
                     run('npm', 'install', cwd=turbolizer_dir)
                     run('npm', 'run-script', 'build', cwd=turbolizer_dir)
+                    # We don't need to deply the node_modules
+                    run('rm', '-rf', turbolizer_dir / node_modules)
                 except Exception as e:
                     print(f'Error occured: {e}')
 
