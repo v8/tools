81ccca1693f171ae78bbc8fe1c2b2d206876edf5
V8 Tools Updater <cbruni@chromium.org>
2021-12-29 05:16:45 +0000

    Deploying to gh-pages from main @ a335bbf1fee006517440e263466118389b0e07e1 🚀

--- head/turbolizer/build/turbolizer.js
+++ head/turbolizer/build/turbolizer.js
@@ -5857,6 +5857,8 @@
   var saturday = weekday(6);
 
   var sundays = sunday.range;
+  var mondays = monday.range;
+  var thursdays = thursday.range;
 
   var month = newInterval(function(date) {
     date.setDate(1);
@@ -5946,6 +5948,8 @@
   var utcSaturday = utcWeekday(6);
 
   var utcSundays = utcSunday.range;
+  var utcMondays = utcMonday.range;
+  var utcThursdays = utcThursday.range;
 
   var utcMonth = newInterval(function(date) {
     date.setUTCDate(1);
