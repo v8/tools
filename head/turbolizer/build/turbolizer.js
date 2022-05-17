0959fdea7a67fe121ae28ba83322a06da52fcb84
V8 Tools Updater <cbruni@chromium.org>
2022-05-17 17:28:20 +0000

    V8 Tools Update

--- head/turbolizer/build/turbolizer.js
+++ head/turbolizer/build/turbolizer.js
@@ -5860,6 +5860,8 @@
   var saturday = weekday(6);
 
   var sundays = sunday.range;
+  var mondays = monday.range;
+  var thursdays = thursday.range;
 
   var month = newInterval(function(date) {
     date.setDate(1);
@@ -5949,6 +5951,8 @@
   var utcSaturday = utcWeekday(6);
 
   var utcSundays = utcSunday.range;
+  var utcMondays = utcMonday.range;
+  var utcThursdays = utcThursday.range;
 
   var utcMonth = newInterval(function(date) {
     date.setUTCDate(1);
